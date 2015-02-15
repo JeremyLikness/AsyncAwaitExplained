af40d17b40e8a73a8d0e61f10b2b172a8fbd5258
Jeremy Likness <jeremy@jeremylikness.com>
2015-02-15 12:01:19 -0500

    Add example with wait()

---
+++ src/08NoAsyncAwait/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace _08NoAsyncAwait
+{
+    class Program
+    {
+        private const string PrideAndPrejudice = "http://www.gutenberg.org/cache/epub/1342/pg1342.txt";
+
+        private static void Main()
+        {
+            Console.WriteLine("Going to read a book.");
+            var client = new HttpClient();
+            var task = client.GetStringAsync(PrideAndPrejudice);
+            task.Wait();
+            if (task.IsCanceled)
+            {
+                Console.WriteLine("Didn't work out.");
+            }
+            else
+            {
+                var book = task.Result;
+                Console.WriteLine("Read {0} bytes.", book.Length);
+            }
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ src/08NoAsyncAwait/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("08NoAsyncAwait")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("08NoAsyncAwait")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3752e7ca-a7f6-43d6-b9ac-1ac1ddb6e1ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
